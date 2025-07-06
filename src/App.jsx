ec0b7ab0c094e4f6e061d14d45fe37e4121ca59e
Prashant <prashant.pandey@app-scoop.com>
2025-07-06 14:43:16 +0530

    redo styles

--- src/App.jsx
+++ src/App.jsx
@@ -13,29 +13,33 @@ function App() {
     <>
       <h1>Full Name Display</h1>
       <form onSubmit={onSubmit}>
-        <label name="firstName">First Name:</label>
+        <label name="firstName">First Name: </label>
         <input
           value={firstName}
           onChange={(event) => setFirstName(event.target.value)}
+          type="text"
           required
           id="firstName"
           className="firstName"
+          style={{ margin: 8 }}
         ></input>
         <br />
-        <label name="lastName">Last Name:</label>
+        <label name="lastName">Last Name: </label>
         <input
           value={lastName}
           onChange={(event) => setLastName(event.target.value)}
+          type="text"
           required
           id="lastName"
           className="lastName"
+          style={{ margin: 8 }}
         ></input>
         <br />
-        <button type="submit" id="submit">
+        <button type="submit" id="submit" style={{ marginTop: "20px" }}>
           Submit
         </button>
       </form>
-      {fullName && <p>Full Name: {fullName}</p>}
+      {fullName && <p>Full Name : {fullName}</p>}
     </>
   );
 }
